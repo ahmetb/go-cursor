dd3ae65cd052f99e26d6e9b4f309ce3b080b3dd4
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-10-08 23:03:24 -0700

    extract Sprintf to helper func

--- cursor.go
+++ cursor.go
@@ -7,90 +7,94 @@ import "fmt"
 
 var Esc = "\x1b"
 
+func escape(format string, args ...interface{}) string {
+	return fmt.Sprintf("%s%s", Esc, fmt.Sprintf(format, args...))
+}
+
 // MoveTo returns ANSI escape sequence to move cursor
 // to specified position on screen.
 func MoveTo(line, col int) string {
-	return fmt.Sprintf("%s[%d;%dH", Esc, line, col)
+	return escape("[%d;%dH", line, col)
 }
 
 // MoveUp returns ANSI escape sequence to move cursor
 // up n lines.
 func MoveUp(n int) string {
-	return fmt.Sprintf("%s[%dA", Esc, n)
+	return escape("[%dA", n)
 }
 
 // MoveDown returns ANSI escape sequence to move cursor
 // down n lines.
 func MoveDown(n int) string {
-	return fmt.Sprintf("%s[%dB", Esc, n)
+	return escape("[%dB", n)
 }
 
 // MoveRight returns ANSI escape sequence to move cursor
 // right n columns.
 func MoveRight(n int) string {
-	return fmt.Sprintf("%s[%dC", Esc, n)
+	return escape("[%dC", n)
 }
 
 // MoveLeft returns ANSI escape sequence to move cursor
 // left n columns.
 func MoveLeft(n int) string {
-	return fmt.Sprintf("%s[%dD", Esc, n)
+	return escape("[%dD", n)
 }
 
 // MoveUpperLeft returns ANSI escape sequence to move cursor
 // to upper left corner of screen.
 func MoveUpperLeft(n int) string {
-	return fmt.Sprintf("%s[%dH", Esc, n)
+	return escape("[%dH", n)
 }
 
 // MoveNextLine returns ANSI escape sequence to move cursor
 // to next line.
 func MoveNextLine() string {
-	return fmt.Sprintf("%sE", Esc)
+	return escape("E")
 }
 
 // ClearLineRight returns ANSI escape sequence to clear line
 // from right of the cursor.
 func ClearLineRight() string {
-	return fmt.Sprintf("%s[0K", Esc)
+	return escape("[0K")
 }
 
 // ClearLineLeft returns ANSI escape sequence to clear line
 // from left of the cursor.
 func ClearLineLeft() string {
-	return fmt.Sprintf("%s[1K", Esc)
+	return escape("[1K")
 }
 
 // ClearEntireLine returns ANSI escape sequence to clear current line.
 func ClearEntireLine() string {
-	return fmt.Sprintf("%s[2K", Esc)
+	return escape("[2K")
 }
 
 // ClearScreenDown returns ANSI escape sequence to clear screen below.
 // the cursor.
 func ClearScreenDown() string {
-	return fmt.Sprintf("%s[0J", Esc)
+	return escape("[0J")
 }
 
 // ClearScreenUp returns ANSI escape sequence to clear screen above.
 // the cursor.
 func ClearScreenUp() string {
-	return fmt.Sprintf("%s[1J", Esc)
+	return escape("[1J")
 }
 
 // ClearEntireScreen returns ANSI escape sequence to clear the screen.
 func ClearEntireScreen() string {
-	return fmt.Sprintf("%s[2J", Esc)
+	return escape("[2J")
 }
 
 // SaveAttributes returns ANSI escape sequence to save current position
 // and attributes of the cursor.
 func SaveAttributes() string {
-	return fmt.Sprintf("%s7", Esc)
+	return escape("7")
 }
 
 // RestoreAttributes returns ANSI escape sequence to restore saved position
 // and attributes of the cursor.
 func RestoreAttributes() string {
-	return fmt.Sprintf("%s8", Esc)
+	return escape("8")
 }
